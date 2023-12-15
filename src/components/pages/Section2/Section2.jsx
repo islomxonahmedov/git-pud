440ee39e84575eb06406d402489d5159b5c8f547
islom <islomxonahmedov7@gmail.com>
2023-12-15 09:10:31 +0500

    Section2 tugadi

--- src/components/pages/Section2/Section2.jsx
+++ src/components/pages/Section2/Section2.jsx
@@ -1,6 +1,12 @@
+import Style from "./Section2.module.css"
 function Section2() {
   return (
-    <div>Section2</div>
+    <div className={Style.section2}>
+      <h1>Select project,</h1>
+      <p>check dependencies, checkout branch, view</p>
+      <p>readme.txt, install tools, run build, run test,</p>
+      <h1>start coding.</h1>
+    </div>
   )
 }
 
--- src/components/pages/login/login.jsx
+++ src/components/pages/login/login.jsx
@@ -1,7 +1,25 @@
+import Style from "./login.module.css"
+import { NavLink } from "react-router-dom"
 function Login() {
   return (
-    <div>login</div>
-  )
+    <div className={Style.container}>
+      <div className={Style.box1}>
+        <h1>Welcome !</h1>
+        <h3>Material Login Form #02</h3>
+        <h4>Designed By <span>SayedFchy</span></h4>
+      </div>
+      <div className={Style.box2}>
+        <h1>Login</h1>
+        <div className={Style.div}></div>
+        <input className={Style.input1} type="email" placeholder="Enter Username" />
+        <input className={Style.input2} type="email" placeholder="Enter Password" />
+        <button className={Style.button1}>LOGIN</button><br />
+        <button className={Style.button2}>Don't Have Account? Signup</button>
+        <a className={Style.forget}href="#"></a>
+        <NavLink className={Style.forget} to="/">Forgot</NavLink>
+      </div>
+    </div>
+      )
 }
 
 export default Login
